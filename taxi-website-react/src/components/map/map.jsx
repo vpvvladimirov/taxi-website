21b961a63f3929e357ca0cc2ec21ad54f98addf4
Vladimir <vladiv291@gmail.com>
2024-03-11 08:40:37 +0200

    Fixed some design

--- taxi-website-react/src/components/map/map.jsx
+++ taxi-website-react/src/components/map/map.jsx
@@ -4,7 +4,8 @@ import React from 'react';
 const MapComponent = () => {
   return (
     <div id='map-container'>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.3958681019544!2d23.361152376592738!3d42.69533791385305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa858c6033a3af%3A0x6aabc4f5f3bb9cd5!2sO.K.%20Supertrans!5e0!3m2!1sen!2sbg!4v1708503686230!5m2!1sen!2sbg" width="850" height="700" frameBorder="0" loading='async' referrerPolicy="no-referrer-when-downgrade"></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2932.3958681019544!2d23.361152376592738!3d42.69533791385305!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40aa858c6033a3af%3A0x6aabc4f5f3bb9cd5!2sO.K.%20Supertrans!5e0!3m2!1sen!2sbg!4v1708503686230!5m2!1sen!2sbg"
+        width="850" height="700" frameBorder="0" loading='async' referrerPolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 };
--- taxi-website-react/src/components/order-form/order-form.jsx
+++ taxi-website-react/src/components/order-form/order-form.jsx
@@ -9,45 +9,60 @@ const OrderForm = () => {
 
   return (
     <div id="order-taxi-form-container">
-      <form onSubmit={handleSubmit} id='signup-form' method='post'>
-        <div className="form-group">
-          <label htmlFor="pickupAddress">Pickup Location:</label>
-          <input
-            value={orderData.pickupAddress}
-            onChange={handleChange}
-            type="text"
-            id="pickupAddress"
-            name="pickupAddress"
-          />
+      {trips.length > 0 ? (
+        <div>
+          <h1 id='accepted-trips-text'>Accepted Trips</h1>
+          <div id='accepted-trips-list'>
+            <table id='accepted-trips-table'>
+              <thead>
+                <th>Driver</th>
+                <th>Pickup Address</th>
+                <th>Destination</th>
+                <th>Action</th>
+              </thead>
+              <tbody>
+                {trips.map(trip => (
+                  <>
+                    <br />
+                    <tr key={trip.activeTripID}>
+                      <td>Driver: {trip.driverID}</td>
+                      <td>Pickup Address: {trip.pickupAddress}</td>
+                      <td>Destination: {trip.dropoffAddress}</td>
+                      <td>
+                        <button id='accept-driver-button'>Accept Driver</button>
+                      </td>
+                    </tr>
+                  </>
+                ))}
+              </tbody >
+            </table>
+          </div>
         </div>
-        <div className="form-group">
-          <label htmlFor="dropoffAddress">Destination:</label>
-          <input
-            value={orderData.dropoffAddress}
-            onChange={handleChange}
-            type="text"
-            id="dropoffAddress"
-            name="dropoffAddress"
-          />
-        </div>
-        <button id='create-trip-button' type="submit">Order Taxi</button>
-      </form>
-      <div>
-        <h1>Accepted Trips</h1>
-        <ul id='accepted-trips-list'>
-          {trips.length > 0 ? (
-            trips.map(trip => (
-              <li key={trip.activeTripID}>
-                <div>Driver: {trip.driverID}</div>
-                <div>Pickup Address: {trip.pickupAddress}</div>
-                <div>Destination: {trip.dropoffAddress}</div>
-              </li>
-            ))
-          ) : (
-            <li>No active trips</li>
-          )}
-        </ul>
-      </div>
+      ) : (
+        <form onSubmit={handleSubmit} id='signup-form' method='post'>
+          <div className="form-group">
+            <label htmlFor="pickupAddress">Pickup Location:</label>
+            <input
+              value={orderData.pickupAddress}
+              onChange={handleChange}
+              type="text"
+              id="pickupAddress"
+              name="pickupAddress"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="dropoffAddress">Destination:</label>
+            <input
+              value={orderData.dropoffAddress}
+              onChange={handleChange}
+              type="text"
+              id="dropoffAddress"
+              name="dropoffAddress"
+            />
+          </div>
+          <button id='create-trip-button' type="submit">Order Taxi</button>
+        </form>
+      )}
     </div>
   );
 }
